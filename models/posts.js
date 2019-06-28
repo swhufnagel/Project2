6ffac935fdb44f6a317bcfa4b58a450e3224000d
Rich <rjw12498@gmail.com>
2019-06-27 20:56:01 -0600

    initial db setup, table and db. No seed

---
+++ models/posts.js
@@ -0,0 +1,35 @@
+module.exports = function(sequelize, DataTypes) {
+    var postTable = sequelize.define("postTable", {
+      postId: {
+        type:DataTypes.INTEGER,
+        autoIncrement : true,
+        primaryKey : true
+      },
+      text: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          len: [0,255]
+        }
+  
+      },
+      image : {
+          type: DataTypes.BLOB,
+          allowNull : true,
+
+      }
+     
+      
+    });
+
+    postTable.associate = function(models){
+        postTable.belongsTo(models.userId, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    }
+
+    return postTable;
+  };
+  
