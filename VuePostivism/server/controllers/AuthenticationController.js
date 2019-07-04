bdd1960aed166ee3d9731b5eb260b8a9fc3b2e1e
Sean Huff <swhufnagel@gmail.com>
2019-07-04 00:35:36 -0600

    rebuilt front end

--- VuePostivism/server/controllers/AuthenticationController.js
+++ VuePostivism/server/controllers/AuthenticationController.js
@@ -1,10 +1,10 @@
-var { userLogin } = require('../models');
+var models = require('../models');
 
 module.exports = {
     register:  async function(req, res) {
         try {
-            var user = await userLogin.create(req.body)
-            res.send(userLogin.toJSON)
+            var account = await models.create(req.body)
+            res.send(account.toJSON)
         } catch (err) {
             res.status(400).send({
                 error: 'This email account is already in use'
--- VuePostivism/server/routes.js
+++ VuePostivism/server/routes.js
@@ -1,18 +1,14 @@
-//import { register } from "./controllers/AuthenticationController";
+var controller = require("./controllers/AuthenticationController");
 // export default function (app) {
-//app.post("/#/",register);
+
 // }
 const path = require("path");
 
 module.exports = function(app){
   app.get("/test", (req, res)=> res.json({ msg: "IT WORKED"}));
-
-
-
-
-
-
-
+  app.post("/api/account",function(req,res){
+    console.log("response", req + " " + res);
+  });
   app.get("*", (req, res)=> {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
   });
