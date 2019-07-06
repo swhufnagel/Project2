7ad7998c0e5517a77fc92d93036a391fab0c0b5c
Sean Huff <swhufnagel@gmail.com>
2019-07-06 12:06:36 -0600

    idk

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+var favicon = require("serve-favicon");
 var db = require("./models");
 
 var app = express();
@@ -11,7 +11,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 // Handlebars
 app.engine(
   "handlebars",
